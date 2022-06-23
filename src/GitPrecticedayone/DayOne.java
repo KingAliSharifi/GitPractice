d4875e77fa11179d5dbadfbb3f7f33ca17575a86
KingAliSharifi <wahidsharifi75@gmail.com>
2022-06-22 23:13:26 -0700

    wahidpractice

--- src/GitPrecticedayone/DayOne.java
+++ src/GitPrecticedayone/DayOne.java
@@ -5,10 +5,8 @@ public class DayOne {
 	public static void main(String[] args) {
 	
 		
-		String m="Kingkha";
-		int t=10;
+		System.out.println("wahid");
 		
-		System.out.println(m+ " get " +t+ " per hour");
 		
 	}
 
